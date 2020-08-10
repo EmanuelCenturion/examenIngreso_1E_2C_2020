cb7bef8d865a29f0325dc952169dc7665976f7e7
EmanuelCenturion <emanuelcenturion82@gmail.com>
2020-08-10 19:47:06 -0300

    examen

--- dos.js
+++ dos.js
@@ -5,7 +5,73 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar()
-{
-  alert("dos");
+*/
+
+
+function mostrar() {
+
+  let marca;
+  let precio;
+  let pesoKilogramo;
+  let tipo;
+  let respuesta = 's';
+  let acumPrecioSolido = 0;
+  let acumPesoLiquido = 0;
+  let pesoTotal;
+  let precioLiquido;
+  let flagLiquido = 0;
+  let PrecioSolido;
+  let flagSolido = 0;
+  let marcaLiquido;
+  let marcaSolido;
+
+  do {
+    do {
+      marca = prompt("Ingrese marca de producto");
+    } while (marca == "");
+    do {
+      precio = parseFloat(prompt("Ingrese precio de producto"));
+    } while (precio <= 0 || isNaN(precio));
+    do {
+      pesoKilogramo = parseFloat(prompt("Ingrese peso por Kilogramo de producto"));
+    } while (pesoKilogramo <= 0 || isNaN(pesoKilogramo));
+    do {
+      tipo = prompt("Ingrese tipo de producto. (solido o liquido)");
+    } while (tipo != "solido" && tipo != "liquido");
+    respuesta = prompt("Desea ingresar otro producto? s/n");
+
+    switch (tipo) {
+      case "solido":
+        acumPrecioSolido += pesoKilogramo;
+        if (precio < PrecioSolido || flagSolido == 0) {
+          PrecioSolido = precio;
+          marcaSolido = marca;
+          flagSolido = 1;
+        }
+        break;
+      case "liquido":
+        acumPesoLiquido += pesoKilogramo
+        if (precio > precioLiquido || flagLiquido == 0) {
+          precioLiquido = precio;
+          marcaLiquido = marca;
+          flagLiquido = 1;
+        }
+        break;
+    }
+    pesoTotal = acumPrecioSolido + acumPesoLiquido;
+
+  } while (respuesta == 's');
+
+  document.write("A)El peso total de la compra es: " + pesoTotal + "<br>");
+  if (marcaLiquido == null) {
+    document.write("B)No se ingreso ningun producto liquido " + "<br>");
+  } else {
+    document.write("B)La marca del liquido mas caro es: " + marcaLiquido + "<br>");
+  }
+  if (marcaSolido == null) {
+    document.write("B)No se ingreso ningun producto solido " + "<br>");
+  } else {
+    document.write("C)La marca del solido mas liviano es: " + marcaSolido + "<br>");
+  }
+
 }
--- uno.js
+++ uno.js
@@ -7,7 +7,65 @@ b)la edad promedio en total.
 c)el hombre mas pesado.
 pedir datos por prompt y mostrar por document.write o console.log
 */
-function mostrar()
-{
-	alert("uno");
+function mostrar() {
+	let nombre;
+	let peso;
+	let sexo;
+	let edad;
+	let contMujeres = 0;
+	let contHombres = 0;
+	let promEdad;
+	let edadTotal;
+	let flagPeso = 0;
+	let pesoHombre;
+	let acumEdadMujeres = 0;
+	let acumEdadHombres = 0;
+
+
+	for (let i = 0; i < 5; i++) {
+		do {
+			nombre = prompt("Ingrese su nombre");
+		} while (nombre == "");
+		do {
+			peso = parseFloat(prompt("Ingrese su peso"));
+		} while (peso <= 0 || isNaN(peso));
+		do {
+			sexo = prompt("Ingrese su sexo. 'f' o 'm'").toLocaleLowerCase();
+		} while (sexo != 'f' && sexo != 'm');
+		do {
+			edad = parseInt(prompt("Ingrese su edad"));
+		} while (edad <= 0 || isNaN(edad));
+
+		switch (sexo) {
+			case 'f':
+				contMujeres++;
+				acumEdadMujeres += edad;
+				break;
+			case 'm':
+				acumEdadHombres += edad;
+				contHombres++;
+				if (peso > pesoHombre || flagPeso == 0) {
+					pesoHombre = peso;
+					flagPeso = 1;
+				}
+				break;
+		}
+
+	}
+
+	promEdad = (acumEdadHombres + acumEdadMujeres) / (contMujeres + contHombres);
+
+	if (contMujeres == 0) {
+		document.write("A)No se ingresaron mujeres");
+	} else {
+		document.write("A)La cantidad de mujeres es: " + contMujeres + "<br>");
+	}
+
+	document.write("B)La edad promedio en total es: " + promEdad.toFixed(2) + "<br>");
+
+	if (contHombres == 0) {
+		document.write("A)No se ingresaron hombres");
+	} else {
+		document.write("C)El hombre mas pesado es: " + pesoHombre + "<br>");
+	}
 }
